48a607065e3c3e388c59e06c3f863fec581900db
iButcat <Butcat@protonmail.com>
2020-08-08 19:08:49 +0200

    config

--- src/main/java/com/example/bug_tracker/repository/UserExtraRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bug_tracker.repository;
-
-import com.example.bug_tracker.model.UserExtra;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserExtraRepository extends JpaRepository<UserExtra, Long> {
-}
