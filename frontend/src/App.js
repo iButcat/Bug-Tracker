0d2bd53227520382a0fe2ec06af0bd8827c44bec
iButcat <Butcat@protonmail.com>
2020-08-14 15:07:09 +0200

    prepare front structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import { CookiesProvider } from 'react-cookie';
 
 import './App.css';
 
+// components
+import Navbar from './components/Navbar';
+
+
+// pages
+import Login from './pages/Login';
+import Register from './pages/Register';
 import Home from './pages/Home';
 
 export default class App extends Component {
   render() {
     return (
-      <CookiesProvider>
         <Home />
-      </CookiesProvider>
     )
   }
 }
